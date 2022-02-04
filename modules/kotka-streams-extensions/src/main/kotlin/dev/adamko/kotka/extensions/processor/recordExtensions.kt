1586341b68b509b082716c22b19831e65f89fb6d
Adam <897017+aSemy@users.noreply.github.com>
2022-02-04 17:05:32 +0100

    fix visibility

--- modules/kotka-streams-extensions/src/main/kotlin/dev/adamko/kotka/extensions/processor/recordExtensions.kt
+++ modules/kotka-streams-extensions/src/main/kotlin/dev/adamko/kotka/extensions/processor/recordExtensions.kt
@@ -2,6 +2,6 @@ package dev.adamko.kotka.extensions.processor
 
 import org.apache.kafka.streams.processor.api.Record
 
-private operator fun <K, V> Record<K, V>.component1(): K = key()
-private operator fun <K, V> Record<K, V>.component2(): V = value()
-private operator fun <K, V> Record<K, V>.component3(): Long = timestamp()
+operator fun <K, V> Record<K, V>.component1(): K = key()
+operator fun <K, V> Record<K, V>.component2(): V = value()
+operator fun <K, V> Record<K, V>.component3(): Long = timestamp()
