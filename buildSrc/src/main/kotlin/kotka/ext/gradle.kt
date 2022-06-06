39e87b743b0f2ff799afea3cd19e83db251f92c0
Adam <897017+aSemy@users.noreply.github.com>
2022-06-06 11:29:51 +0200

    try implementing Maven Central publishing... (signing still doesn't work)

---
+++ buildSrc/src/main/kotlin/kotka/ext/gradle.kt
@@ -0,0 +1,20 @@
+package kotka.ext
+
+import org.gradle.api.Action
+import org.gradle.api.artifacts.repositories.PasswordCredentials
+import org.gradle.api.provider.Provider
+import org.gradle.api.provider.ProviderFactory
+
+
+// https://github.com/gradle/gradle/issues/20925
+fun ProviderFactory.credentialsAction(
+  repositoryName: String
+): Provider<Action<PasswordCredentials>> = zip(
+  gradleProperty("${repositoryName}Username"),
+  gradleProperty("${repositoryName}Password"),
+) { user, pass ->
+  Action<PasswordCredentials> {
+    username = user
+    password = pass
+  }
+}
