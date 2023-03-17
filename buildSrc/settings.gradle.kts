cece4b4485eaba249a1227045e29bea9abfa1a4f
Adam <897017+aSemy@users.noreply.github.com>
2023-03-17 02:24:12 +0100

    make Gradle repositories more declarative - re-using them between the project and buildSrc isn't necessary

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -1,9 +1,21 @@
 rootProject.name = "buildSrc"
 
-apply(from = "./repositories.settings.gradle.kts")
+pluginManagement {
+  repositories {
+    gradlePluginPortal()
+    mavenCentral()
+  }
+}
 
+@Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
-  @Suppress("UnstableApiUsage")
+  repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+
+  repositories {
+    mavenCentral()
+    gradlePluginPortal()
+  }
+
   versionCatalogs {
     create("libs") {
       from(files("../gradle/libs.versions.toml"))
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,20 @@
 rootProject.name = "kotka-streams"
 
+pluginManagement {
+  repositories {
+    gradlePluginPortal()
+    mavenCentral()
+  }
+}
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+  repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+
+  repositories {
+    mavenCentral()
+  }
+}
 
 include(
   ":docs",
@@ -12,10 +27,3 @@ include(
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
-
-apply(from = "./buildSrc/repositories.settings.gradle.kts")
-
-@Suppress("UnstableApiUsage") // Central declaration of repositories is an incubating feature
-dependencyResolutionManagement {
-  repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
-}
